f7f601c27478ba1f253720cf6ebe11464354f1b8
Sap-04-Learner <saumyadeep2004@gmail.com>
2025-07-11 22:18:36 +0530

    data_processing 3

--- MLOPS-PROJECT-1/utils/common_functions.py
+++ MLOPS-PROJECT-1/utils/common_functions.py
@@ -19,3 +19,12 @@ def read_yaml(file_path):
     except Exception as e:
         logger.error('Error while reading YAML file')
         raise CustomException("Failed to read YAML file", e)
+    
+
+def load_data(path):
+    try:
+        logger.info('Loading data')
+        return pd.read_csv(path)
+    except Exception as e:
+        logger.error(f'Error loading the data: {e}')
+        raise CustomException('Failed to load data', e)
